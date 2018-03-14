e6d6149151e90227b2f14224537d366bdc3d46d8
Bob Paauwe <bpaauwe@yahoo.com>
2018-03-14 10:05:53 -0700

    Add copyright header.

--- rtl2udp.c
+++ rtl2udp.c
@@ -1,3 +1,29 @@
+/*
+ * rtl2udp  Copyright (C) 2018 Robert Paauwe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This program translates Acuite sensor data output from the rtl_433
+ * program, adds barometric sensor data from a BMP280 and light level
+ * sensor data from a TSL2561 and broadcast the data on port 50222 using
+ * UDP.
+ */
 #define _GNU_SOURCE
 
 #include <stdio.h>
